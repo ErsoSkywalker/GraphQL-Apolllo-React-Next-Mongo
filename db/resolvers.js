1deefa3c215387204b0cc0dbb2c34029a1c58be4
Lynch <contiverosv@outlook.com>
2020-07-13 11:10:28 -0500

    Se le ha agregado un Query para obtener Usuarios y una Mutarion para agregarlos

--- db/resolvers.js
+++ db/resolvers.js
@@ -6,7 +6,16 @@ const Usuario = require('../models/usuarios');
 //Configuramos los Resolvers, estos tienen que estar antes en el Schema
 const resolvers = {
    Query:{
-       obtenerCurso : () => "Algo"
+       obtenerCurso : () => "Algo",
+       obtenerUsuarios: async (_, { input })=>{
+           const existeUsuario = await Usuario.find();
+           if(!existeUsuario[0]){throw new Error('No hay usuarios disponibles');}
+           try {
+               return Usuario.find();
+           } catch (error) {
+               console.log(error);
+           }
+       }
    },
    Mutation : {
        nuevoUsuario : async (_, { input }) => {
@@ -17,8 +26,10 @@ const resolvers = {
             if(existeUsuario){throw new Error('Ya está registrado ese Usuario'); }
                 
             try {
+                //Creamos una instancia del objeto y luego le damos .save, solo para insertarlo en una nueva collection
                 const usuario = new Usuario(input);
                 usuario.save();
+                //Nos va a regresar el usuario, ya que en el gql así lo especificamos
                 return usuario;
             } catch (error) {
                 console.log(error);
--- db/schema.js
+++ db/schema.js
@@ -21,6 +21,7 @@ const typeDefs = gql`
 
     type Query{
         obtenerCurso : String
+        obtenerUsuarios : [Usuario]
     }
 
     type Mutation{
